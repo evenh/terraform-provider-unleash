ecb8caef9fd3ec8f1064b19cd23c19644c7d56a9
Even Holthe <even.holthe@me.com>
2020-06-08 11:32:27 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/evenh/terraform-provider-unleash
+
+go 1.14
+
+require github.com/hashicorp/terraform-plugin-sdk v1.13.1
---
+++ unleash/provider.go
@@ -0,0 +1,11 @@
+package unleash
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{},
+	}
+}
