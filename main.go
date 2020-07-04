2f208ddc9c9b7d7debf13a4ef9adf1fd3d86d2e0
Even Holthe <even.holthe@me.com>
2020-07-04 23:36:10 +0200

    Remove old manual testing

--- main.go
+++ main.go
@@ -13,20 +13,4 @@ func main() {
 			return unleash.Provider()
 		},
 	})
-
-	/*client, err := api.NewClient("http://localhost:4242/api", "terraform-provider-unleash", &api.UnsecureAuthentication{Email: "even.holthe@me.com"})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	features, err := client.ListFeatureFlags()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, f := range features {
-		log.Printf("Feature[name=%s, description=%s, enabled=%s]", f.Name, f.Description, f.Enabled)
-	}*/
 }
